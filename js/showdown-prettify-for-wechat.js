c40bcf579d660f181dfc827856e71a4a16193c2b
gxyz <18710890823@163.com>
2017-05-11 23:52:03 +0800

    change some style

--- js/showdown-prettify-for-wechat.js
+++ js/showdown-prettify-for-wechat.js
@@ -19,9 +19,9 @@
       filter: function (source) {
         return source.replace(/(<pre[^>]*>)?[\n\s]?<code([^>]*)>/gi, function (match, pre, codeClass) {
           if (pre) {
-            return '<pre class="prettyprint linenums" style="font-size: 12px;line-height: 14px"><code' + codeClass + ' style="font-size: 12px;line-height: 14px">';
+            return '<pre class="prettyprint linenums" style="font-size: 10px;line-height: 10px"><code' + codeClass + ' style="font-size: 10px;line-height: 10px">';
           } else {
-            return ' <code class="prettyprint code-in-text"  style="font-size: 14px;line-height: 18px">';
+            return ' <code class="prettyprint code-in-text"  style="font-size: 12px;line-height: 14px">';
           }
         });
       }
